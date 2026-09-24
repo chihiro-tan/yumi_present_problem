13e9c8e203725b26d7f14b2577e30fe8e3caf913
chihiro-tan <chihiro-tan@users.noreply.github.com>
2026-09-24 23:25:40 +0900

    Add randomized quiz progress saving

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='patho-quiz-v1';
+const CACHE='patho-quiz-v2';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
